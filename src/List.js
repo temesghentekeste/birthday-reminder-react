08fa0973e8f13b6f2fd08d680e8f7f36d5552e93
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-10-16 01:37:43 +0300

    Add birthday list component

--- src/List.js
+++ src/List.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const List = () => {
+const List = ({people}) => {
   return (
     <>
-      <h2>list component</h2>
+      {
+        people.map( person => {
+          const { id, name, image, age } = person
+          return(
+            <article key={id} className="person">
+                <img src={image} alt={name}/>
+                 <h4>{name}</h4>
+                 <div>
+                  <p>{age}</p>
+                </div>
+            </article>
+          )
+        })
+      }
     </>
   );
 };
